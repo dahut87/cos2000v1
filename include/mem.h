cea146350675084a694a9aaa9fe4ac51fc1c5255
Nicolas Hordé <nicolas@palon.fr>
2004-11-19 18:54:51 +0000

    feat: ajout des definitions CE (compatible executable)

--- include/mem.h
+++ include/mem.h
@@ -1,11 +1,23 @@
-MB	struc
-Check 	         dw 'NH'
-IsNotLast 	db 0
-IsResident	db 0
-Reference	dw 0
-Sizes		dw 0
-Names		db 24 dup (0)
+MB	struc                    ;Bloc de mémoire
+Check 	         dw 'NH'         ;signature du bloc de mémoire
+IsNotLast 	db 0             ;flag indiquant le dernier bloc
+IsResident	db 0             ;flag indiquant que le bloc est resident
+Reference	dw 0             ;pointeur vers le bloc parent
+Sizes		dw 0             ;taille du bloc en paragraphe de 16 octet
+Names		db 24 dup (0)    ;nom du bloc
 MB	ends
 
-Free  equ 0
-memorystart equ 1000h
+exe	struc                    ;Executable COS
+Checks 	         db "CE"         ;signature de l'exe
+major            db 1            ;N° version majeur
+minor            db 0            ;N° version mineur
+checksum         dd 0            ;Checksum de l'exe
+compressed       db 0            ;a 1 si compressé par RLE
+import           dw 0            ;importation de fonctions
+export           dw 0            ;exportation de fonctions
+blocs          dw 0              ;sections des blocs mémoire
+exe	ends
+
+
+Free        equ 0                 ;Reference quand libre
+memorystart equ 1000h             ;premier bloc de la mémoire
