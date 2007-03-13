dcae05febe30693cf41607adb21665f0c14f52a4
Nicolas Hordé <nicolas@palon.fr>
2007-03-13 20:44:48 +0000

    feat: rajout d'une structure pour le passage en mode FLAT

--- include/mem.h
+++ include/mem.h
@@ -1,23 +1,32 @@
-MB	struc                    ;Bloc de mémoire
-Check 	         dw 'NH'         ;signature du bloc de mémoire
-IsNotLast 	db 0             ;flag indiquant le dernier bloc
-IsResident	db 0             ;flag indiquant que le bloc est resident
-Reference	dw 0             ;pointeur vers le bloc parent
-Sizes		dw 0             ;taille du bloc en paragraphe de 16 octet
-Names		db 24 dup (0)    ;nom du bloc
-MB	ends
+struc mb	                    ;Bloc de mémoire
+check 	        db "NH"         ;signature du bloc de mémoire
+isnotlast 	db 0             ;flag indiquant le dernier bloc
+isresident	db 0             ;flag indiquant que le bloc est resident
+reference	dw 0             ;pointeur vers le bloc parent
+sizes		dw 0             ;taille du bloc en paragraphe de 16 octet
+names		db 24 dup (0)    ;nom du bloc
+ends mb	
 
-exe	struc                    ;Executable COS
-Checks 	         db "CE"         ;signature de l'exe
-major            db 1            ;N° version majeur
-minor            db 0            ;N° version mineur
+struc exe	                    ;Executable COS
+checks 	         db "CE"         ;signature de l'exe
+major            db 1            ;N° version
 checksum         dd 0            ;Checksum de l'exe
 compressed       db 0            ;a 1 si compressé par RLE
-import           dw 0            ;importation de fonctions
-export           dw 0            ;exportation de fonctions
-blocs          dw 0              ;sections des blocs mémoire
-exe	ends
+exports          dw 0            ;importation de fonctions
+imports          dw 0            ;exportation de fonctions
+sections         dw 0            ;sections des blocs mémoire
+starting         dw 15
+ends exe	
+
+struc descriptor
+limit_low   dw 0
+base_low    dw 0
+base_middle db 0
+dpltype     db 0
+limit_high  db 0
+base_high   db 0
+ends descriptor
 
 
-Free        equ 0                 ;Reference quand libre
+free        equ 0                 ;Reference quand libre
 memorystart equ 0052h             ;premier bloc de la mémoire
