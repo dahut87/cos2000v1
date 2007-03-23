48fd1d4ca326de5f1855b85f24b1750f448b98ef
Nicolas Hordé <nicolas@palon.fr>
2007-03-23 16:27:16 +0000

    feat: mise à jour du code pour compilation sur LZASM en stdcall Passage au format CE, offset a 0 Déclaration des imports et exports en utilisant les macros

--- contribs/ex-tasm.asm
+++ contribs/ex-tasm.asm
@@ -3,17 +3,29 @@
 Smart				;Optimisations
 .code				;Segment de code
 
-org 0100h			;Point d'entré en 0100h
+org 0h			    ;Point d'entré en 0h
+
+checks 	         db "CE"           ;signature de l'exe
+major            db 1              ;N° version
+checksum         dd 0              ;Checksum de l'exe
+compressed       db 0              ;a 1 si compressé par RLE
+exports          dw 0              ;importation de fonctions
+imports          dw imported       ;exportation de fonctions
+sections         dw 0              ;sections des blocs mémoire
+starting         dw realstart
+
+imported:
+db "VIDEO.LIB::print",0
+print dd 0
 
 start:
-	mov	ah,0Dh		
-	mov	si,offset msg
-	int	47h		;Afficher le texte (Showstring0)
-	xor	ax,ax
-	int	16h		;Attendre l'appuie sur une touche
-	db	0CBH            ;retour far
+    push   msg
+    call   far [cs:print]		;Afficher le texte (Showstring0)
+	xor	   ax,ax
+    int	   16h		        ;Attendre l'appuie sur une touche
+	db	   0CBH             ;retour far
 
-msg db 'Hello World',0
+msg db 'Hello World !!',0
 
 end start
 
