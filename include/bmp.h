c6db694ba2ac536490da882de92f8cf1363c9a03
Nicolas Hordé <nicolas@palon.fr>
2007-03-25 00:58:01 +0000

    feat: mise en minuscule de l'ensemble du fichier

--- include/bmp.h
+++ include/bmp.h
@@ -1,40 +1,40 @@
-BMP_File         		struc
-BMP_FileType            db      'BM'
-BMP_FileSize            dd      ?       ; taille du fichier
-BMP_Reserved            dd      0       ; toujours 0
-BMP_BitMapOffset        dd      ?       ; offset de l'image
-BMP_HeaderSize          dd      ? ; taille de l'entete en octects
-BMP_Width               dd      ? ; largeur en pixels de l'image
-BMP_Height              dd      ? ; hauteur en pixels de l'image
-BMP_Planes              dw      1 ; nombre de plan utilisés
-BMP_BitsPerPixel        dw      ? ; nombre de bits par pixels
-BMP_Compression         dd      ? ; méthode de compression 
-BMP_SizeOfBitMap        dd      ? ; taille de l'image en octects
-BMP_HorzResolution      dd      ? ; resolution horizontale en pixels par mètre
-BMP_VertResolution      dd      ? ; resolution verticale en pixels  par mètre
-BMP_ColorsUsed          dd      ? ; nombre de couleur dans la palette  si 0: palette entière si BitPerPixel<=8
-BMP_ColorsImportant     dd      ? ; nombre de couleurs importantes masques pour les modes de plus de 8 bits par pixels
-BMP_RedMask             dd      ?
-BMP_GreenMask           dd      ?
-BMP_BlueMask            dd      ?
-BMP_AlphaMask           dd      ?
-BMP_ColorSpaceType      dd      ?
-BMP_RedX                dd      ?
-BMP_RedY                dd      ?
-BMP_RedZ                dd      ?
-BMP_GreenX              dd      ?
-BMP_GreenY              dd      ?
-BMP_GreenZ              dd      ?
-BMP_BlueX               dd      ?
-BMP_BlueY               dd      ?
-BMP_BlueZ               dd      ?
-BMP_GammaRed            dd      ?
-BMP_GammaGreen          dd      ?
-BMP_GammeBlue           dd      ?
-BMP_file       		ends
+struc bmp_file         		
+bmp_filetype            db      'bm'
+bmp_filesize            dd      ?       ; taille du fichier
+bmp_reserved            dd      0       ; toujours 0
+bmp_bitmapoffset        dd      ?       ; offset de l'image
+bmp_headersize          dd      ? ; taille de l'entete en octects
+bmp_width               dd      ? ; largeur en pixels de l'image
+bmp_height              dd      ? ; hauteur en pixels de l'image
+bmp_planes              dw      1 ; nombre de plan utilisés
+bmp_bitsperpixel        dw      ? ; nombre de bits par pixels
+bmp_compression         dd      ? ; méthode de compression 
+bmp_sizeofbitmap        dd      ? ; taille de l'image en octects
+bmp_horzresolution      dd      ? ; resolution horizontale en pixels par mètre
+bmp_vertresolution      dd      ? ; resolution verticale en pixels  par mètre
+bmp_colorsused          dd      ? ; nombre de couleur dans la palette  si 0: palette entière si bitperpixel<=8
+bmp_colorsimportant     dd      ? ; nombre de couleurs importantes masques pour les modes de plus de 8 bits par pixels
+bmp_redmask             dd      ?
+bmp_greenmask           dd      ?
+bmp_bluemask            dd      ?
+bmp_alphamask           dd      ?
+bmp_colorspacetype      dd      ?
+bmp_redx                dd      ?
+bmp_redy                dd      ?
+bmp_redz                dd      ?
+bmp_greenx              dd      ?
+bmp_greeny              dd      ?
+bmp_greenz              dd      ?
+bmp_bluex               dd      ?
+bmp_bluey               dd      ?
+bmp_bluez               dd      ?
+bmp_gammared            dd      ?
+bmp_gammagreen          dd      ?
+bmp_gammeblue           dd      ?
+ends bmp_file       		
 
-;BMP_Compression peut prendre les valeurs suivantes: 
-BMP_COMP_UNCOMP         equ     0       ; pas de compression
-BMP_COMP_RLE8           equ     1       ; 8-bit run length encoding
-BMP_COMP_RLE4           equ     2       ; 4-bit tun length encoding
-BMP_COMP_BFE            equ     3       ; bitfields encoding
+;bmp_compression peut prendre les valeurs suivantes: 
+bmp_comp_uncomp         equ     0       ; pas de compression
+bmp_comp_rle8           equ     1       ; 8-bit run length encoding
+bmp_comp_rle4           equ     2       ; 4-bit tun length encoding
+bmp_comp_bfe            equ     3       ; bitfields encoding
