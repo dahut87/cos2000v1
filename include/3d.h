8b89273f46c326de8d27c1a52f642228cc9858fb
Nicolas Hordé <nicolas@palon.fr>
2007-03-31 19:21:23 +0000

    feat: ajout des structures "vertex3d" et "mat" nécessaire a la 3D

---
+++ include/3d.h
@@ -0,0 +1,32 @@
+struc vertex3d 
+tx dd ?
+ty dd ?
+tz dd ?
+ends vertex3d 
+
+struc mat 
+p1 dd ?
+p2 dd ?
+p3 dd ?
+p4 dd ?
+p5 dd ?
+p6 dd ?
+p7 dd ?
+p8 dd ?
+p9 dd ?
+p10 dd ?
+p11 dd ?
+p12 dd ?
+p13 dd ?
+p14 dd ?
+p15 dd ?
+p16 dd ?
+ends mat 
+
+main   equ 4D4Dh
+edit   equ 3D3Dh
+object equ 4000h
+mesh   equ 4100h
+vertex equ 4110h
+face   equ 4120h
+locale equ 4160h
